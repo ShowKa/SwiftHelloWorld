3ea27d078fccba6dab74a95aeef623b309ce24fe
ShowKa <johndoe@example.com>
2017-03-09 23:02:15 +0900

    set up project

--- SwiftHelloWorld/AppDelegate.swift
+++ SwiftHelloWorld/AppDelegate.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 import CoreData
+import Simperium
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let splitViewController = self.window!.rootViewController as! UISplitViewController
